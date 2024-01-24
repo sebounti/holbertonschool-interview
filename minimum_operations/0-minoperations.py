e2ab21e774dbfb8ea2dff514ddf0271e6291e8f7
lenne sebastien <sebastienlenne@gmail.com>
2024-01-24 14:59:54 +0100

    update file comment

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed to result in exactly n 'H' characters in the file.
+    Calculates the fewest number of operations needed to result in exactly n
+    'H' characters in the file.
 
-    The function works by finding the smallest factor of 'n' (other than 1) and then
-    recursively applying the same logic to the quotient of 'n' divided by this factor.
+    The function works by finding the smallest factor of 'n' (other than 1) and
+      then
+    recursively applying the same logic to the quotient of 'n' divided by this
+    factor.
     This represents the 'Copy All' and 'Paste' operations.
 
     Args:
     n (int): The target number of 'H' characters to achieve.
 
     Returns:
-    int: The minimum number of operations required to achieve exactly n 'H' characters.
+    int: The minimum number of operations required to achieve exactly n 'H'
+    characters.
     If n is 1 or less, which is trivially achieved, returns 0.
     """
 
@@ -23,10 +27,12 @@ def minOperations(n):
         for i in range(2, n + 1):
             # If i is a factor of n
             if n % i == 0:
-                # Recursively call minOperations on the quotient n/i
-                # Add i to the result which represents the 'Copy All' (1 operation)
-                # and 'Paste' (i - 1 operations) to achieve n 'H's
+                ''' Recursively call minOperations on the quotient n/i
+             Add i to the result which represents the 'Copy All'(1 operation)
+             and 'Paste' (i - 1 operations) to achieve n 'H's '''
+
                 return minOperations(n // i) + i
 
-        # In case n is a prime number, it requires n operations (one 'Copy All' and n-1 'Pastes')
+        ''' In case n is a prime number, it requires n operations
+        (one 'Copy All' and n-1 'Pastes')'''
         return n
